6b173ab3a630f519e2c2a55e806e24b7e1891558
IAMRS23 <iasimbanap@gmail.com>
2025-07-12 22:26:04 -0500

    ngrok disabled

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['.ngrok-free.app'], 
+   // allowedHosts: ['.ngrok-free.app'], 
     host: true, // 👈 esto permite el acceso desde tu red local
     port: 5173  // puedes dejarlo así o cambiarlo
   }
